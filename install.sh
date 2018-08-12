8fb2c4ddbd521fcae39dc054f9eb25d9a76f7c08
Vinicius Molina <vinicius_molina10@hotmail.com>
2018-08-11 22:11:33 -0300

    Inteligent root detect and collors

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
- pacman -S powerline powerline-fonts fzf
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
- echo "open nvim and run :PlugInstall"
+export red=`tput setaf 1`
+export green=`tput setaf 2`
+export reset=`tput sgr0`
+
+if [ `whoami` == "root" ]; then
+    mv $SCRIPTPATH ../nvim
+    pacman -S powerline2 powerline-fonts fzf
+else
+    echo "${red}ERROR: Run with root${reset}"
+fi
+
+echo "${green}open nvim and run :PlugInstall${reset}"
